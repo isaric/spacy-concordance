3384dd1e44100e888eb9c21bf36dcc9d361131b9
Ivan Šarić <ivan@path-variable.com>
2022-05-12 13:56:05 +0200

    initial commmit

---
+++ README.md
@@ -0,0 +1,13 @@
+This sample script uses SpaCy to generate a concordance from a text file.
+A concordance is an alphabetically sorted list of all unique words occurring in 
+the text along with the frequency of their appearance and the 1-based index of the 
+sentences they appear in. The path to the text file is defined as the first argument
+when calling the script. The script will output the concordance to stdout. 
+It was written using Python 3.8.10
+
+Example usage:
+
+    python concordance.py my-text-file.txt
+
+SpaCy is the only dependency needed to run the script. The version used is written in
+the requirements.txt file located inside the project.
---
+++ concordance.py
@@ -0,0 +1,27 @@
+import spacy
+from collections import Counter
+
+nlp = spacy.load("en_core_web_sm", disable=['ner', 'parser', 'tagger'])
+nlp.add_pipe('sentencizer')
+doc = nlp(open("/home/isaric/check.txt").read())
+
+words_sentence = [(sent_id, token.text.lower()) for sent_id, sent in enumerate(doc.sents) for token in sent if ("\n" not in token.text and not token.is_punct)]
+sentence_map = dict()
+for t in words_sentence:
+    si_list = str(sentence_map.get(t[1]) if t[1] in sentence_map else '')
+    si_list = si_list + "," if si_list else si_list
+    si_list = si_list + f'{t[0] + 1}'
+    sentence_map.update({t[1]: si_list})
+
+freq_map = Counter([w[1] for w in words_sentence])
+sorted_keys = sorted(freq_map.keys())
+letter_range = [*range(ord('a'), ord('z') + 1)]
+
+for i in range(0, len(sorted_keys)):
+    c = i if len(letter_range) > i else i % len(letter_range)
+    times = int(i) / len(letter_range)
+    times = 1 if times < 1 else 1 + int(round(times,0))
+    letter_ordinal = ''
+    for j in range(0,times):
+        letter_ordinal = letter_ordinal + chr(letter_range[c])
+    print(f'{letter_ordinal}.\t{sorted_keys[i]}\t{{{freq_map.get(sorted_keys[i])}:{sentence_map.get(sorted_keys[i])}}}')
